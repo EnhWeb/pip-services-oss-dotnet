d7de456b4cde2ab6fe41acd4d71373dd3295acbf
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2018-03-22 22:32:00 -0700

    Added OssFactory

--- src/Build/OssFactory.cs
+++ src/Build/OssFactory.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using PipServices.Commons.Build;
+using PipServices.Commons.Refer;
+using PipServices.Oss.ElasticSearch;
+
 namespace PipServices.Oss.Build
 {
-    public class OssFactory
+    public class OssFactory: Factory
     {
+        public static readonly Descriptor Descriptor = new Descriptor("pip-services", "factory", "logger", "default", "1.0");
+        public static readonly Descriptor ElasticSearchLoggerDescriptor = new Descriptor("pip-services", "logger", "elasticsearch", "*", "1.0");
+
         public OssFactory()
         {
+            RegisterAsType(OssFactory.ElasticSearchLoggerDescriptor, typeof(ElasticSearchLogger));
         }
     }
 }
